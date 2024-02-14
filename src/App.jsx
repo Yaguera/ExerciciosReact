3d5d1799d250e9fc14fc300f55ac8d5d4b137cbb
Yaguera <ya-gotre@hotmail.com>
2024-02-14 19:21:40 -0300

    Refactor search component and update mock data
    - Move Search component to exercicio1 folder
    - Replace food list with mock user data
    - Implement search by full name or last name in Search component
    - Add MOCK_DATA.json with user data
    - Remove old Search component file
    - Update package.json to replace lint and preview scripts with watch script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
-import Search from './Pages/Search'
+import Search from './Pages/Exercicio1/Search'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Lista from './pages/Lista'
---
+++ src/pages/exercicio1/Search.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useState } from 'react'
+import JSONDATA from '../../MOCK_DATA.json'
+
+const Search = () => {
+    const data = JSONDATA
+      
+      
+      const [filterData, setFilterData] = useState(data)
+      
+      const handleSearch = (e) =>{
+        const searchTerm = e.target.value.toLowerCase();
+        console.log(searchTerm)
+    
+        if (searchTerm === "") {
+          setFilterData(data);
+          return;
+        }
+        
+      
+        const filteredData = data.filter(
+          (item) => {
+            let nomeCompleto = item.first_name.toLowerCase() + " " + item.last_name.toLowerCase();
+            return nomeCompleto.startsWith(searchTerm) || item.last_name.toLowerCase().startsWith(searchTerm)  ;
+          }
+        );
+        setFilterData(filteredData);
+      };
+    
+      return (
+        <div className='m-16 flex justify-center items-center '>
+            <div className="p-6 app pt-5 border-2 max-w-80">
+              <div>
+                Search: <input className='border-2' name="query" type="text" onChange={handleSearch} />
+              </div>
+            <div className="list pt-5 flex flex-col flex-wrap gap-3 max-h-80 items-center text-left overflow-x-auto">
+                {filterData &&
+                  filterData.map((item, index) => (
+                    <div key={index}>{item.first_name} {item.last_name}</div> //Display each item
+                  ))}
+            </div>
+            </div>
+        </div>
+      )
+    }
+
+export default Search;
